d887a30a439efe4e4c64fb2a38ed001342a3452d
namdt <namdt1@vnpay.vn>
2024-01-30 11:50:33 +0700

    update sing-in and sign-up with @clerk/nextjs auth

---
+++ app/(auth)/layout.js
@@ -0,0 +1,18 @@
+import { Inter } from "next/font/google";
+import { ClerkProvider as Provider } from "@clerk/nextjs";
+import "../globals.css";
+export const metadata = {
+  title: "Auth",
+  description: "Next.js 14 App",
+};
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({ children }) {
+  return (
+    <Provider>
+      <html lang='en'>
+        <body className={`${inter.className} bg-purple-1`}>{children}</body>
+      </html>
+    </Provider>
+  );
+}
